a840cb3ee3333309ca5238ef74cb9dfee5d5429b
JoelNieto <joenieto90@hotmail.com>
2020-09-17 11:19:12 -0500

    Finished

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const searchAPI = () => {
-    const API_URL = "https://pokeapi.co/api/v2/pokemon/"
-    let idPokemon = document.getElementById("idPokemon").value
-    let require = new Request(`${API_URL}${idPokemon}`)
+document.addEventListener('DOMContentLoaded', () => searchAPI())
 
-    fetch(require)
-        .then(response => response.json())
-        .then(data => {
-            cardData(data)
-            console.log(data)
-        })
+const searchAPI = async () => {
+    const API_URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150"
+    let require = new Request(`${API_URL}`)
 
-        .catch(err => console.log(err))
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    cardData(data)
 }
 
-const cardData = (data) => {
-    const hability = data.abilities
-    //console.log(hability)
-    let arrayHability = []
+const cardData = async (data) => {
 
-    for(let i in hability) {
-        arrayHability.push(' ' + hability[i].ability.name)
-        //console.log(arrayHability[i])
+    for(let i = 0; i < data.results.length; i++){
+        fetch(data.results[i].url)
+            .then(response => response.json())
+            .then(pokemon => {
+                return dataPokemon(pokemon)
+            })
+            .catch(err => console.log(err))
     }
 
+    const dataPokemon = (pokemon) => {
 
-    //Aqui estoy indicandole que me pegue en el HTMl los valores de id y el name del repo
-    repos.innerHTML += `
-    <img class="imgPokemon" src="${data.sprites.front_default}" alt="${data.name.toUpperCase()}">
-    <div class="textPokemon">
-        <p>Id: <span>${data.id}</span></p>
-        <p>Nombre: <span>${data.name.toUpperCase()}</span></p>
-        <p>Habilidad:<span>${arrayHability}</span></p>
-        <p>Experiencia: <span>${data.base_experience}</span></p>
-        <p>Peso: <span>${data.weight} kg</span></p>
-    </div>
-    `
+        const hability = pokemon.abilities
+        //console.log(hability)
+        let arrayHability = []
+
+        for(let i in hability) {
+            arrayHability.push(' ' + hability[i].ability.name)
+            //console.log(arrayHability[i])
+        }
+
+        const name = pokemon.name[0].toUpperCase() +pokemon.name.slice(1)
+
+        //Aqui estoy indicandole que me pegue en el HTMl los valores de id y el name del repo
+        repos.innerHTML += `
+        <div class="pokemon-Card">
+            <div class="img-container">
+                <img class="imgPokemon" src="${pokemon.sprites.front_default}" alt="${name}" loading="lazy">
+            </div>
+
+            <div class="textPokemon">
+                <p class="idPokemon"><span>#${pokemon.id.toString().padStart(3, '0')}</span></p>
+                <p>Nombre: <span>${name}</span></p>
+                <p>Habilidad:<span>${arrayHability}</span></p>
+                <p>Experiencia: <span>${pokemon.base_experience}</span></p>
+                <p>Peso: <span>${pokemon.weight} kg</span></p>
+            </div>
+        </div>
+        `
+    }
 }
